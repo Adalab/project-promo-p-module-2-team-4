1eb44c1e6d6d76091cf276bb8bb2a48648a933e6
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-03-30 19:59:03 +0200

    add js class in html

---
+++ src/js/color_preview.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//CONSTANTES del PREVIEW.HTML
+//Constantes palette colors
+const palette1 = document.querySelector ('.radio01');
+const palette2  = document.querySelector ('.radio02');
+const palette3 = document.querySelector ('.radio03');
+
+//Constantes campos para cumplimentar
+const nameLastname = document.querySelector ('.js_cardName');
+const description = document.querySelector ('.js_cardJob');
+
+//Constantes elementos estéticos
+const elementBorder = document.querySelector ('.js_cardInfo');
+
+//Constantes circulo iconos redes sociales
+const circleIcons = document.querySelector ('.js_cardIcon');
+
+//Constantes icons redes sociales
+const rrssIcons = document.querySelector ('.js_cardLink');
+
+
