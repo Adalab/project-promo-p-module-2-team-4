136bf89d90cf7238d2a5a96f26d9698d723b8c80
miranda-zapata <miranda.zapata@gmail.com>
2022-03-30 14:26:01 +0200

    add var layout

--- src/js/form.js
+++ src/js/form.js
@@ -1,20 +1,4 @@
 'use strict';
-
-// Constantes DISEÑA
-const designLegend = document.querySelector('.js_designLegend');
-const design = document.querySelector('.js_design');
-const arrowDesign = document.querySelector('.js_arrowDesign');
-
-// Constantes RELLENA
-const fillLegend = document.querySelector('.js_fillLegend');
-const fill = document.querySelector('.js_fill');
-const arrowFill = document.querySelector('.js_arrowFill');
-
-// Constantes COMPARTE
-const shareLegend = document.querySelector('.js_shareLegend');
-const share = document.querySelector('.js_share');
-const arrowShare = document.querySelector('.js_arrowShare');
-
 // Colapsar DISEÑA
 designLegend.addEventListener('click', () => {
   design.classList.toggle('collapsed');
