145240be8f8f3475b204ac3267432af1a71c8baf
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-04-01 18:47:12 +0200

    fix a little problem with js

--- src/js/color_preview.js
+++ src/js/color_preview.js
@@ -5,30 +5,32 @@
 const radioButton1 = document.querySelector('.js_radio-1');
 const radioButton2 = document.querySelector('.js_radio-2');
 const radioButton3 = document.querySelector('.js_radio-3');
-const previewContainer = document.querySelector('.js_preview');
+const previewContainerCard = document.querySelector('.js_preview');
 
 
-function handleClickRadio1 (event) {
-  previewContainer.classList.add('palette-1');
+function handleClickRadio1(event) {
+  previewContainerCard.classList.remove('palette-2');
+  previewContainerCard.classList.remove('palette-3');
+
+  previewContainerCard.classList.add('palette-1');
 }
-function handleClickRadio2 (event) {
-  previewContainer.classList.add('palette-2');
+
+function handleClickRadio2(event) {
+  previewContainerCard.classList.remove('palette-1');
+  previewContainerCard.classList.remove('palette-3');
+
+  previewContainerCard.classList.add('palette-2');
 }
-function handleClickRadio3 (event) {
-  previewContainer.classList.add('palette-3');
+
+function handleClickRadio3(event) {
+  previewContainerCard.classList.remove('palette-1');
+  previewContainerCard.classList.remove('palette-2');
+
+  previewContainerCard.classList.add('palette-3');
 }
 
 
 radioButton1.addEventListener('click', handleClickRadio1);
 radioButton2.addEventListener('click', handleClickRadio2);
 radioButton3.addEventListener('click', handleClickRadio3);
-
-//OPCIÓN "dificil" IVAN
-/*const allRadioButtons = document.querySelectorAll('.js_radio');
-
-
-for( const  eachRadioButton   of    allRadioButtons   ) {
-  eachRadioButton.addEventListener('click', handleClickRadioUnique);
-}*/
-
 
