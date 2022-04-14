38ef8daf757c239a9f3569f1fa08b85793b78ff9
graciagarpin <graciagarpin@gmail.com>
2022-04-14 13:59:07 +0200

    quick fix format and remove comments

--- src/js/p_createCard.js
+++ src/js/p_createCard.js
@@ -4,17 +4,12 @@
 const buttonCreateCard = document.querySelector('.js_buttonCreateCard');
 const containerDone = document.querySelector('.js_containerDone');
 
-
-
 // Funciones con "una" única acción
 function createCard() {
-
   buttonCreateCard.classList.add('buttonCreateCard--sent');
 
   containerDone.classList.remove('collapsed');
-
 }
-
 
 // Función manejadora
 function handleClickButtonCreateCard(event) {
@@ -26,32 +21,35 @@ function handleClickButtonCreateCard(event) {
 // Las Constantes de los elementos de la tarjeta están en g_fill.js
 
 function ifCardCanBeCreate() {
-
-  if (data.name !== '' && data.job !=='' && data.phone !== '' && data.email !== '' && data.linkedin !== '' && data.github !== '' && data.photo !== '') {
+  if (
+    data.name !== '' &&
+    data.job !== '' &&
+    data.phone !== '' &&
+    data.email !== '' &&
+    data.linkedin !== '' &&
+    data.github !== '' &&
+    data.photo !== ''
+  ) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
 function validationFormFields() {
   let html = '';
-  if(ifCardCanBeCreate()){
-    // console.log('bien hecho');
+  if (ifCardCanBeCreate()) {
     html += `<h2 class="containerDone__title">¡Bien hecho! Tu tarjeta ha sido creada</h2>`;
-    html +=  `<a href="" class="containerDone__link"
+    html += `<a href="" class="containerDone__link"
     >https://awesome-profile-card.com?id=A456DF0001</a>`;
     html += `<button class="buttonShare">
     <i class="fa-brands fa-twitter"></i> Compartir en Twitter
   </button>`;
-  
-  }else{
-    // console.log('ojo');
+  } else {
     html = `<h2 class="containerDone__title">¡Ojo! Rellena todo el formulario</h2>`;
   }
   containerCreateCard.innerHTML = html;
 }
-
 
 // Evento
 buttonCreateCard.addEventListener('click', handleClickButtonCreateCard);
